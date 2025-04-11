eb882fee87fe87590f6b3d49578ce0b00cbd71d9
divyanshiroy <158273266+divyanshiroy@users.noreply.github.com>
2025-04-12 01:44:33 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import flwr as fl
+import os
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     strategy = fl.server.strategy.FedAvg()
-    fl.server.start_server(server_address='0.0.0.0:8080', strategy=strategy)
+    port = os.environ.get("PORT", 10000)  # Default to 10000 if PORT not set
+    fl.server.start_server(server_address=f"0.0.0.0:{port}", strategy=strategy)
